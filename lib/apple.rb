f373de344fca8eab4ec8d0d3af10cc0befd4b3fa
axton21 <axton_n@yahoo.com>
2015-02-02 19:07:02 -0500

    labs

--- lib/apple.rb
+++ lib/apple.rb
@@ -1,9 +1,26 @@
 def apple_picker_with_select(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just has apples in it
+  	basket = []
+  	fruit_list.select do |x|
+  		if x == "apple"
+  			basket << "apple"
+  		end
+  	end
+
+
 end
 
 def apple_picker_with_collect(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # like the challenge above, you want to return an array that has just the apples in it
+	basket = []
+	  	fruit_list.collect do |x|
+	  		#["apple", "orange", "apple"]
+	  		if x == "apple"
+	  			basket << "apple"
+	  		end
+#	  		basket
+	  	end
+	  		basket
 end
--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
@@ -6,7 +9,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :new_years => ["Party Hats"]
   #   },
   #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
+    #     :fourth_of_july => ["Fireworks", "BBQ"]
   #   },
   #   :fall => {
   #     :thanksgiving => ["Turkey"]
@@ -16,26 +19,36 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holiday(holiday_hash, supply)
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to either the
   # Christmas or the New Year's arrays
+holiday_hash[:winter][:new_years] << supply 
+holiday_hash[:winter][:christmas] << supply 
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  newholiday = holiday_hash
+  newholiday[season][holiday_name] = supply_array
+  newholiday
+# => holday supplies, season, day, 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  bucket = holiday_hash[:winter][:christmas] + holiday_hash[:winter][:new_years]
+  bucket
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -46,9 +59,49 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  # binding.pry
+holiday_hash.each do |key, season|
+    puts key.to_s.capitalize + ":"
+    season.each do |season , event|
+      capitalized(season, event)
+    end
+  end
+end
+
+
+def capitalized(season, event)
+    newevent = event.join(", ")
+    a = season.to_s.split("_").map(&:capitalize).join(" ")
+    puts "  "+ a + ": "+ newevent
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bucket = []
+  holiday_hash.each do |holiday, season|
+  #season  is a hash winter => xmas, etc
+  season.each do |event, party|
+    # event = xmas party = bbq
+    # party.each do |x,y|
+      if party.include?("BBQ")
+        bucket << event
+        end
+      end
+    end
+  bucket
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
